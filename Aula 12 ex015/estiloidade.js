4737d4114caad0eff8330385a2f211b4faf681c8
Flaviana <flavianasanto1@gmail.com>
2022-07-29 19:44:54 -0300

    exercício

---
+++ Aula 12 ex015/estiloidade.js
@@ -0,0 +1,5 @@
+var ano = window.document.getElementById('idade')
+var sexo = window.document.getElementById('')
+var age = Number(ano.value)
+var res = window.document.getElementById('res')
+res.innerHTML = `Detectamos ${} com ${} anos.`
